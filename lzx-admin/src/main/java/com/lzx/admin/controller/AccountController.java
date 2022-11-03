f9156ddd8150fab936bf5bc83ebf68ddabf382b7
linzongxing <973565601@qq.com>
2022-11-03 22:59:57 +0800

    修改原因：整合mybatis修改bug

--- lzx-admin/src/main/java/com/lzx/admin/controller/AccountController.java
+++ lzx-admin/src/main/java/com/lzx/admin/controller/AccountController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @author lzx
@@ -17,6 +18,7 @@ public class AccountController {
     @Autowired
     AccountServiceImp accountServiceImp;
 
+    @ResponseBody
     @GetMapping("/acct")
     public Account getById(@RequestParam("id") Long id) {
         return accountServiceImp.getAcctById(id);
