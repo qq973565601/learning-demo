42e02905369197ce695b4f354878001908fd680c
linzongxing <973565601@qq.com>
2022-11-15 00:06:07 +0800

    修改原因：postpre测试链接

--- lzx-admin/src/main/java/com/lzx/admin/LzxAdminApplication.java
+++ lzx-admin/src/main/java/com/lzx/admin/LzxAdminApplication.java
@@ -2,10 +2,11 @@ package com.lzx.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @ServletComponentScan(basePackages = "com.lzx.admin")
-@SpringBootApplication
+@SpringBootApplication(exclude = RedisAutoConfiguration.class)
 public class LzxAdminApplication {
 
     public static void main(String[] args) {
---
+++ lzx-module/src/main/java/cn/lzx/postgre/bean/Person.java
@@ -0,0 +1,15 @@
+package cn.lzx.postgre.bean;
+
+import lombok.Data;
+
+/**
+ * @author lzx
+ * @since 2022/11/14
+ */
+@Data
+public class Person {
+
+    private int id;
+    private String name;
+    private int age;
+}
